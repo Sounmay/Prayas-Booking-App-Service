d3749cd72dcba3d2db2f4c82d88d48843db94a90
naiyar2000 <naiyarimam.2000@gmail.com>
2021-07-27 12:53:19 +0530

    firebase storage partially implemented

--- lib/Providers/FirebaseApi.dart
+++ lib/Providers/FirebaseApi.dart
@@ -12,4 +12,5 @@ class FirebaseApi {
       return null;
     }
   }
+
 }
--- lib/Screens/ParlourScreens/FinalEditPage.dart
+++ lib/Screens/ParlourScreens/FinalEditPage.dart
@@ -1,8 +1,12 @@
 import 'dart:io';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:freelance_booking_app_service/Providers/database.dart';
+import 'package:path/path.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:freelance_booking_app_service/Models/ParlourDetails.dart';
+import 'package:freelance_booking_app_service/Providers/FirebaseApi.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:marquee/marquee.dart';
 
@@ -17,8 +21,12 @@ class _FinalEditPageState extends State<FinalEditPage> {
   String ownerImagepath = '';
   File file;
 
+  bool isEditMode = false;
+
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     final _arguments =
         ModalRoute.of(context).settings.arguments as Map<dynamic, dynamic>;
     Location _location = _arguments["location"];
@@ -47,7 +55,7 @@ class _FinalEditPageState extends State<FinalEditPage> {
                           bottomRight: Radius.circular(10)))),
               Flexible(
                 child: Container(
-                  width: MediaQuery.of(context).size.width * 0.7,
+                  width: width * 0.7,
                   margin: EdgeInsets.only(left: 10),
                   child: Row(children: [
                     Flexible(
@@ -62,15 +70,38 @@ class _FinalEditPageState extends State<FinalEditPage> {
                 ),
               ),
             ]),
+            SizedBox(
+              height: 10,
+            ),
             Container(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color:
+                          isEditMode ? Color(0xff5D5FEF) : Colors.transparent),
+                  borderRadius: BorderRadius.circular(10)),
               height: MediaQuery.of(context).size.height * 0.5,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'Shop Details',
-                    style: TextStyle(color: Color(0xff606572)),
-                  ),
+                  Row(
+                      mainAxisAlignment: isEditMode
+                          ? MainAxisAlignment.spaceBetween
+                          : MainAxisAlignment.center,
+                      children: [
+                        if (isEditMode) Container(),
+                        Text(
+                          'Shop Details',
+                          style: TextStyle(color: Color(0xff606572)),
+                        ),
+                        if (isEditMode)
+                          IconButton(
+                              onPressed: () {
+                                Navigator.of(context).popUntil(
+                                    ModalRoute.withName("/parlourlocation"));
+                              },
+                              icon: Icon(Icons.edit))
+                      ]),
                   SizedBox(
                     height: 10,
                   ),
@@ -78,8 +109,8 @@ class _FinalEditPageState extends State<FinalEditPage> {
                     borderRadius: BorderRadius.circular(8),
                     child: Container(
                       decoration: BoxDecoration(color: Colors.grey[400]),
-                      height: 200,
-                      width: 200,
+                      height: height * 0.22,
+                      width: width * 0.4,
                       child: file != null
                           ? Image.file(
                               File(file.path),
@@ -204,7 +235,7 @@ class _FinalEditPageState extends State<FinalEditPage> {
                     height: 10,
                   ),
                   Container(
-                    width: MediaQuery.of(context).size.width * 0.5,
+                    width: width * 0.45,
                     height: 30,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(8),
@@ -221,8 +252,8 @@ class _FinalEditPageState extends State<FinalEditPage> {
                             size: 20,
                           ),
                           SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.4,
-                            height: 30,
+                            width: MediaQuery.of(context).size.width * 0.35,
+                            height: height * 0.06,
                             child: Marquee(
                                 text: parlourDetails.location.address,
                                 scrollAxis: Axis.horizontal,
@@ -262,24 +293,50 @@ class _FinalEditPageState extends State<FinalEditPage> {
             ),
             Container(
               height: MediaQuery.of(context).size.height * 0.5,
-              margin: EdgeInsets.symmetric(horizontal: 10),
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                      color:
+                          isEditMode ? Color(0xff5D5FEF) : Colors.transparent),
+                  borderRadius: BorderRadius.circular(10)),
               child: Column(
                 children: [
-                  Text('${parlourDetails.details.parlourType} Parlour',
-                      style: TextStyle(
-                          color: Color(0xff5D5FEF),
-                          fontWeight: FontWeight.bold)),
+                  Row(
+                      mainAxisAlignment: isEditMode
+                          ? MainAxisAlignment.spaceBetween
+                          : MainAxisAlignment.center,
+                      children: [
+                        if (isEditMode) Container(),
+                        Text('${parlourDetails.details.parlourType} Parlour',
+                            style: TextStyle(
+                                color: Color(0xff5D5FEF),
+                                fontWeight: FontWeight.bold)),
+                        if (isEditMode)
+                          IconButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .popUntil(ModalRoute.withName("/details2"));
+                              },
+                              icon: Icon(Icons.edit))
+                      ]),
                   SizedBox(
-                    height: 10,
+                    height: height * 0.5 * 0.06,
                   ),
-                  Flexible(
-                      child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                    child: Text(parlourDetails.details.aboutParlour,
-                        style: TextStyle(color: Color(0xff606572))),
-                  )),
+                  ConstrainedBox(
+                    constraints: new BoxConstraints(
+                      minWidth: width * 0.8,
+                      minHeight: height * 0.5 * 0.2,
+                      maxHeight: height * 0.5 * 0.4,
+                    ),
+                    child: Flexible(
+                        child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(parlourDetails.details.aboutParlour,
+                          style: TextStyle(color: Color(0xff606572))),
+                    )),
+                  ),
                   SizedBox(
-                    height: 10,
+                    height: height * 0.5 * 0.06,
                   ),
                   Text('Your Team',
                       style: TextStyle(
@@ -288,20 +345,135 @@ class _FinalEditPageState extends State<FinalEditPage> {
                   SizedBox(
                     height: 10,
                   ),
-                  ...List.generate(
-                      int.parse(parlourDetails.details.numOfEmployees),
-                      (index) => Container(child: Text('hello'))),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/otpscreen');
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    height: MediaQuery.of(context).size.height * 0.5 * 0.4,
+                    width: MediaQuery.of(context).size.width * 0.75,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount:
+                          parlourDetails.details.employeeDetailList.length,
+                      itemBuilder: (BuildContext ctx, index) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(horizontal: 1),
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              new Container(
+                                  width: width * 0.25,
+                                  height: height * 0.1,
+                                  margin: EdgeInsets.only(bottom: 8),
+                                  decoration: new BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      image: new DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: FileImage(File(parlourDetails
+                                              .details
+                                              .employeeDetailList[index]
+                                              .imagefile))))),
+                              new Text(
+                                parlourDetails
+                                    .details.employeeDetailList[index].name,
+                              ),
+                              new Text(
+                                parlourDetails
+                                    .details.employeeDetailList[index].number,
+                                style: TextStyle(color: Color(0xff5D5FEF)),
+                              )
+                            ],
+                          ),
+                        );
                       },
-                      child: Text('Save & Proceed'))
+                    ),
+                  ),
                 ],
               ),
+            ),
+            Column(
+              children: [
+                TextButton(
+                    onPressed: () {
+                      for (int i = 0;
+                          i < parlourDetails.details.employeeDetailList.length;
+                          i++) {
+                        uploadFile(File(parlourDetails
+                            .details.employeeDetailList[i].imagefile));
+                      }
+                      Navigator.pushNamed(context, '/otpscreen');
+                    },
+                    child: SizedBox(
+                      width: width * 0.4,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Looks Good!!',
+                                style: TextStyle(color: Colors.white)),
+                            SizedBox(
+                              width: width * 0.04,
+                            ),
+                            Icon(Icons.thumb_up_alt_outlined,
+                                color: Colors.white),
+                          ]),
+                    ),
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color(0xFF00A676))),
+                SizedBox(
+                  width: width * 0.2,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      setState(() {
+                        isEditMode = !isEditMode;
+                      });
+                    },
+                    style: ButtonStyle(
+                      side: MaterialStateProperty.all(
+                        BorderSide(color: Color(0xff5D5FEF)),
+                      ),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                    ),
+                    child: Text('Edit',
+                        style: TextStyle(color: Color(0xff5D5FEF))),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: height * 0.03,
             )
           ]),
         ),
       ),
     );
+  }
+
+  UploadTask task;
+
+  Future<String> uploadFile(File _file) async {
+    if (_file == null) return '';
+    final fileName = basename(_file.path);
+    final uid = FirebaseAuth.instance.currentUser.uid;
+    final destination = 'serviceImage/$uid/$fileName';
+
+    task = FirebaseApi.uploadFile(destination, _file);
+
+    if (task == null) return '';
+
+    final snapshot = await task.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+
+    // DatabaseService().uploadImage(urlDownload);
+    return urlDownload;
+  }
+}
+
+class MyClip extends CustomClipper<Rect> {
+  Rect getClip(Size size) {
+    return Rect.fromLTWH(0, 0, 100, 100);
+  }
+
+  bool shouldReclip(oldClipper) {
+    return false;
   }
 }
