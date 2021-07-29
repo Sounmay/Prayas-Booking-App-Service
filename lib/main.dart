7dc4c96f36e943a1be7f3ae32c13b60f19752e7a
SnigdhaBarik <snigdhabarik10@gmail.com>
2021-07-30 00:30:20 +0530

    Snig. Adding service page

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:freelance_booking_app_service/Models/User.dart';
 import 'package:freelance_booking_app_service/Providers/authProvider.dart';
 import 'package:freelance_booking_app_service/Screens/DoctorScreens/DoctorLocation.dart';
 import 'package:freelance_booking_app_service/Screens/OTPScreen.dart';
+import 'package:freelance_booking_app_service/Screens/ParlourScreens/ParlourServices.dart';
 import 'package:freelance_booking_app_service/Screens/ParlourScreens/DetailsSecond.dart';
 import 'package:freelance_booking_app_service/Screens/ParlourScreens/FinalEditPage.dart';
 import 'package:freelance_booking_app_service/Screens/ParlourScreens/ParlourLocation.dart';
@@ -15,7 +16,6 @@ import 'package:freelance_booking_app_service/Screens/Wrapper.dart';
 import 'package:freelance_booking_app_service/Screens/uploadImage.dart';
 import 'package:freelance_booking_app_service/Widgets/Login.dart';
 import 'package:provider/provider.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,7 +45,8 @@ class MyApp extends StatelessWidget {
           '/finalParlourPage': (context) => FinalEditPage(),
           '/details2': (context) => DetailsSecond(),
           '/schedule': (context) => Schedule(),
-          '/otpscreen': (context) => OTPScreen()
+          '/otpscreen': (context) => OTPScreen(),
+          '/ps': (context) => ParlourServices()
         },
       ),
     );
